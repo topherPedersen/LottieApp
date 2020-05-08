fede4406e085d11084be1185595ea124f01ccf33
topherPedersen <chrispedatx@gmail.com>
2020-05-08 11:48:42 -0500

    initial commit

---
+++ App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return(
+      <View>
+        <Text>Hello, Lottie...</Text>
+      </View>
+    );
+  }
+}
+
+export default App;
